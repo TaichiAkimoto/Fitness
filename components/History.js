8326656cce81f7bbdc230d409a1ca3db4c9aded8
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-10-23 17:20:32 +0900

    Save while adding stackNavigation from react-navigation

--- components/History.js
+++ components/History.js
@@ -7,8 +7,13 @@ import { fetchCalendarResults } from '../utils/api'
 import UdaciFitnessCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
   componentDidMount() {
     const { dispatch } = this.props
 
@@ -33,9 +38,12 @@ class History extends Component {
             </Text>
           </View>
         : <TouchableOpacity
-            onPress={() => console.log('Pressed!')}
+            onPress={() => this.props.navigation.navigate(
+              'EntryDetail',
+              { entryId: key }
+            )}
           >
-              <Text>{JSON.stringify(metrics)}</Text>
+              <MetricCard date={formattedDate} metrics={metrics}/>
           </TouchableOpacity>}
     </View>
   )
@@ -51,6 +59,11 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if(ready === false) {
+      return <AppLoading />
+    }
     return (
       <UdaciFitnessCalendar
         items={entries}
